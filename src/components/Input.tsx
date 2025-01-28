e1ee430e55f949dc1b6553fa441e3e8a734b5802
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-01-28 23:54:05 +0200

    feat: create input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,25 @@
+import clsx from "clsx"
+import { ComponentProps } from "react"
+
+interface Props extends ComponentProps<"input"> {
+  placeholder: string
+  label: string
+}
+
+function Input({ label, placeholder, type, className }: Props) {
+  return (
+    <div className="flex flex-col">
+      <label>{label}</label>
+      <input
+        className={clsx(
+          "border rounded-xs bg-gray-100 py-1 border-primary-400 indent-3",
+          className,
+        )}
+        type={type}
+        placeholder={placeholder}
+      />
+    </div>
+  )
+}
+
+export default Input
