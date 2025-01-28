aa65674013b53de615633ab92de059f60a6c1ed3
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-01-28 22:53:11 +0200

    feat: create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css"
+import Header from "./components/Header"
 import Navbar from "./components/Navbar"
 
 export default function App() {
   return (
     <div className="w-7xl m-auto">
       <Navbar />
+      <Header />
     </div>
   )
 }
