7a8a136e6d97067305c99eeab7d8e12eadd8e27e
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-02-02 15:36:51 +0200

    chore: add labels

--- src/components/RadioButtons.tsx
+++ src/components/RadioButtons.tsx
@@ -1,15 +1,20 @@
-function RadioButtons() {
+interface Props {
+  label1: string
+  label2: string
+}
+
+function RadioButtons({ label1, label2 }: Props) {
   return (
-    <>
+    <div className="space-x-4">
       <label>
         <input type="radio" value="option1" name="options" />
-        MySelf
+        {label1}
       </label>
       <label>
         <input type="radio" value="option2" name="options" />
-        Someone
+        {label2}
       </label>
-    </>
+    </div>
   )
 }
 
