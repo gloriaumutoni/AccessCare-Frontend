dd76f91b18f619f16707bf84dbba1c2e42800379
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-02-02 15:21:58 +0200

    feat: create speciality component

---
+++ src/components/Speciality.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router"
+import Input from "./Input"
+
+const specialities = ["Pediatrician", "General Practitioner", "Obstetrician"]
+
+function Speciality() {
+  return (
+    <div className="w-2xs bg-gray-200 border p-4  border-primary-400 rounded-sm">
+      {specialities.map((speciality) => (
+        <label key={speciality} className="flex gap-3">
+          <Input type="checkbox" />
+          {speciality}
+        </label>
+      ))}
+      <Link to="#" className="text-end block text-primary-400 underline">
+        Load More
+      </Link>
+    </div>
+  )
+}
+
+export default Speciality
