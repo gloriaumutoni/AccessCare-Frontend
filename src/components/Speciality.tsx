2e6a3ede30f0f8f0281854d4e0ad37530faa44b2
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-02-02 15:35:28 +0200

    feat: create docto's information component

--- src/components/Speciality.tsx
+++ src/components/Speciality.tsx
@@ -5,9 +5,9 @@ const specialities = ["Pediatrician", "General Practitioner", "Obstetrician"]
 
 function Speciality() {
   return (
-    <div className="w-2xs bg-gray-200 border p-4  border-primary-400 rounded-sm">
+    <div className="w-2xs bg-gray-100 border p-4 border-primary-400 rounded-sm">
       {specialities.map((speciality) => (
-        <label key={speciality} className="flex gap-3">
+        <label key={speciality} className="flex gap-3 ">
           <Input type="checkbox" />
           {speciality}
         </label>
