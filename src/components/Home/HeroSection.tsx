86aa8b6663327a33d0329a596faf9da2f0b1e43d
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-02-05 11:43:35 +0200

    chore: create herosection

---
+++ src/components/Home/HeroSection.tsx
@@ -0,0 +1,35 @@
+import Button from "../Button"
+
+function HeroSection() {
+  return (
+    <div className="flex justify-between mt-12">
+      <div className="w-sm space-y-8 mt-12">
+        <h1 className="font-bold text-4xl">Your Health Comes First.</h1>
+        <p className="text-gray-500">
+          With CareAccess services you will receive the best medical treatment
+          in your home. Our team of skilled medical professionals and aids
+          ensure that you get the care you need and deserve.
+        </p>
+        <Button className="w-3xs py-2">Consult Doctor</Button>
+      </div>
+      <div className="relative px-14">
+        <img className="w-sm" src="/landing_page_nurse.png" alt="Nurse" />
+        <img
+          className="w-32 absolute top-[50%] right-0 "
+          src="/public/patients_number.png"
+          alt="Patients number"
+        />
+      </div>
+      <div className="flex flex-col justify-between">
+        <img className="w-24" src="/public/Arc.png" alt="Arc" />
+        <p className="text-gray-500 w-xs">
+          <span className="text-2xl text-primary-400">“</span>A trusted
+          assistance companion for your health needs.
+          <span className="text-2xl text-primary-400">”</span>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection
