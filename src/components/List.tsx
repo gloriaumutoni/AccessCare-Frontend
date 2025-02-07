24e13d1912c7e97a9d9433f3ba18fd80f003318d
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-02-07 10:08:03 +0200

    refactor: rename prop

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,15 +2,15 @@ import { ComponentProps } from "react"
 
 interface Props extends ComponentProps<"div"> {
   title: string
-  quickLinks: Array<string>
+  list: Array<string>
 }
 
-function List({ quickLinks, title }: Props) {
+function List({ list, title }: Props) {
   return (
     <div>
       <h2 className="text-xl font-bold py-3">{title}</h2>
       <ul className="space-y-2">
-        {quickLinks.map((links) => (
+        {list.map((links) => (
           <li key={links}>{links}</li>
         ))}
       </ul>
