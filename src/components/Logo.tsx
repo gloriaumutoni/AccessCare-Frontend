fecbb17f61dd3f0dffac8edcc899fb322b2dd33d
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-02-13 09:53:22 +0200

    chore: make logo link

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,15 @@
 import clsx from "clsx"
 import { ComponentProps } from "react"
+import { useNavigate } from "react-router"
 
 function Logo({ className }: ComponentProps<"div">) {
+  const navigate = useNavigate()
+  const NavigateToHomePage = () => navigate("/")
   return (
-    <div className={clsx("flex items-center space-x-2", className)}>
+    <div
+      className={clsx("flex items-center space-x-2 cursor-pointer", className)}
+      onClick={NavigateToHomePage}
+    >
       <img src="/image 21 (1).png" className="w-12" />
       <p className="font-bold text-black">CareAccess</p>
     </div>
