2029890f4c8a68af6def5b045681fa64a968877e
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-02-13 19:36:16 +0200

    chore: create error page

---
+++ src/Pages/404.tsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <>
+      <h1 className="text-5xl font-medium">Ooops...</h1>
+      <p className="text-lg">Sorry, an unexpected Error has occured</p>
+    </>
+  )
+}
+
+export default NotFound
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,9 +2,10 @@ import { createBrowserRouter } from "react-router"
 import Home from "../Pages/Home"
 import Bookings from "../Pages/Bookings"
 import Login from "../Pages/Login"
+import NotFound from "../Pages/404"
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
+  { path: "/", element: <Home />, errorElement: <NotFound /> },
   { path: "/bookings", element: <Bookings /> },
   { path: "/login", element: <Login /> },
 ])
