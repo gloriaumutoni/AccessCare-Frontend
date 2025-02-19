6a425f30eb4628accbff763f6bc1de4e668c7b1c
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-02-19 20:15:10 +0200

    chore: create signup page

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -4,26 +4,34 @@ import Logo from "../components/Logo"
 
 function Login() {
   return (
-    <div className="flex min-h-screen">
-      <div className="w-2xl">
-        <img src="/equipments.png" alt="Doctors equipments" />
-      </div>
-      <div>
-        <Logo className="absolute right-12" />
-        <h2 className="font-bold ">Welcome back!</h2>
-        <p>Enter your Credentials to access your account</p>
-        <Input
-          label="Email"
-          placeholder="Enter you email"
-          className="rounded-7xl"
-        />
-        <Input
-          label="Password"
-          placeholder="****"
-          type="password"
-          className="rounded-2xl"
-        />
-        <Button className="w-lg rounded-2xl">Login</Button>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="p-8 rounded-2xl shadow-2xl">
+        <div className="flex flex-col space-y-1 pb-3 items-center justify-center">
+          <Logo />
+          <h2 className="font-bold ">Create a new account</h2>
+          <p className="text-gray-400 text-sm">Enter details to register</p>
+        </div>
+        <div className="space-y-4">
+          <Input
+            label="Full Name"
+            placeholder="Enter your name"
+            className="rounded-md w-xs"
+          />
+          <Input
+            label="Email"
+            placeholder="Enter your email"
+            className="rounded-md w-xs"
+          />
+          <Input
+            label="Password"
+            placeholder="******"
+            type="password"
+            className="rounded-md w-xs"
+          />
+          <div className="flex justify-center">
+            <Button className="w-3xs rounded-lg mt-3 py-1">Login</Button>
+          </div>
+        </div>
       </div>
     </div>
   )
