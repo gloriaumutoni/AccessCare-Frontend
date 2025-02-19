0f283da710f2f3b8d4436368b0d9620180e49e03
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-02-19 21:59:18 +0200

    chore: link pages

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,9 +3,10 @@ import { ComponentProps, PropsWithChildren } from "react"
 
 type Props = PropsWithChildren & ComponentProps<"button">
 
-export default function Button({ className, children }: Props) {
+export default function Button({ className, children, onClick }: Props) {
   return (
     <button
+      onClick={onClick}
       className={clsx("bg-primary-400 cursor-pointer text-white", className)}
     >
       {children}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,10 @@
-import { NavLink } from "react-router"
+import { NavLink, useNavigate } from "react-router"
 import Button from "./Button"
 import Logo from "./Logo"
 import { PATHS } from "../routes/constants"
 
 function Navbar() {
+  const Navigate = useNavigate()
   return (
     <nav className="max-w-7xl mx-auto flex justify-between items-center py-4">
       <Logo />
@@ -13,7 +14,12 @@ function Navbar() {
         <NavLink to={PATHS.bookings}>Find Doctors</NavLink>
         <NavLink to={PATHS.dashboard}>Track your health</NavLink>
       </div>
-      <Button className="px-6 rounded-sm py-1">Register</Button>
+      <Button
+        className="px-6 rounded-sm py-1"
+        onClick={() => Navigate("/login")}
+      >
+        Register
+      </Button>
     </nav>
   )
 }
