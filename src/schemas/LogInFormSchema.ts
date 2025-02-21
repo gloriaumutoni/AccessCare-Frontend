5fd189ef0c7abec537c680b7a7cd8f41573a62e3
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-02-21 11:46:42 +0200

    chore: change min

--- src/schemas/LogInFormSchema.ts
+++ src/schemas/LogInFormSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 
 const LogInFormSchema = z.object({
   email: z.string().email(),
-  password: z.string().min(5).max(20),
+  password: z.string().min(4).max(20),
 })
 
 export default LogInFormSchema
