dcff9041874dec633807d4a074f03850d29c85e7
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-02-22 13:04:59 +0200

    style: add background and text color

--- src/components/dashboard/Card.tsx
+++ src/components/dashboard/Card.tsx
@@ -5,19 +5,25 @@ import { Link } from "react-router"
 interface Props extends ComponentProps<"div"> {
   title: string
   participantsNumber: number
+  LinkColor: string
 }
 
-function DashboardCard({ title, participantsNumber, className }: Props) {
+function DashboardCard({
+  title,
+  participantsNumber,
+  LinkColor,
+  className,
+}: Props) {
   return (
     <div
       className={clsx(
-        "flex flex-col rounded-md w-xs shadow shadow-gray-300",
+        "flex flex-col space-y-2 px-4 py-3 rounded-md w-xs shadow shadow-gray-300 ",
         className,
       )}
     >
-      <h2 className="text-lg">{title}</h2>
-      <h3 className="text-xl">{participantsNumber}</h3>
-      <Link className="text-md" to="">
+      <h2 className="text-lg font-medium">{title}</h2>
+      <h3 className="text-2xl font-bold">{participantsNumber}</h3>
+      <Link className={clsx("text-md font-medium", LinkColor)} to="">
         View details
       </Link>
     </div>
