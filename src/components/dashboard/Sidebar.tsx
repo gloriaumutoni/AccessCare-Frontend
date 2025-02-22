f4bddaf5dbac84b974af09541085ee80fc5b1cab
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-02-22 12:41:14 +0200

    chore: create sidebar component

---
+++ src/components/dashboard/Sidebar.tsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router"
+import Logo from "../Logo"
+import { FaHome, FaUser } from "react-icons/fa"
+import { FaUserDoctor } from "react-icons/fa6"
+import { IoLogOutOutline } from "react-icons/io5"
+import SidebarLink from "./SidebarLink"
+import { PATHS } from "../../routes/constants"
+
+const sidebarLinks = [
+  {
+    path: PATHS.DASHBOARD.ROOT,
+    text: "Home",
+    element: <FaHome />,
+  },
+  {
+    path: PATHS.DASHBOARD.DOCTORS,
+    text: "Doctors",
+    element: <FaUserDoctor />,
+  },
+  {
+    path: PATHS.DASHBOARD.ROOT,
+    text: "Users",
+    element: <FaUser />,
+  },
+  {
+    path: "",
+    text: "Logout",
+    element: <IoLogOutOutline />,
+  },
+]
+
+function Sidebar() {
+  return (
+    <section className="bg-primary-400 text-white flex flex-col w-3xs h-screen text-lg space-y-3 px-8 py-3">
+      <Logo />
+      <hr />
+      <div className="flex flex-col space-y-3">
+        {sidebarLinks.map((links) => (
+          <SidebarLink key={links.path}>
+            <Link to={links.path}>{links.text}</Link>
+            {links.element}
+          </SidebarLink>
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default Sidebar
