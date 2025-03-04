5d32c8bd343c62139218af5147e7463aae9534e8
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-03-04 21:23:14 +0200

    chore: create doctors table

--- src/components/dashboard/DoctorsContent.tsx
+++ src/components/dashboard/DoctorsContent.tsx
@@ -1,61 +1,63 @@
-const DoctorsContent = () => {
+import Table from "@mui/material/Table"
+import TableBody from "@mui/material/TableBody"
+import TableContainer from "@mui/material/TableContainer"
+import TableHead from "@mui/material/TableHead"
+import TableRow from "@mui/material/TableRow"
+import Paper from "@mui/material/Paper"
+import { StyledTableCell, StyledTableRow } from "./table"
+import { doctors } from "../../data/doctors"
+
+interface CreateDataProps {
+  Id: number
+  First_name: string
+  Last_name: string
+  Specialist: string
+  Gender: string
+  Location: string
+}
+;[]
+
+function createData({
+  Id,
+  First_name,
+  Last_name,
+  Specialist,
+  Gender,
+  Location,
+}: CreateDataProps) {
+  return { Id, First_name, Last_name, Specialist, Gender, Location }
+}
+const rows = doctors.map((doctor) => createData({ ...doctor }))
+
+export default function DoctorsContent() {
   return (
-    <div className="p-6 bg-white rounded-lg shadow">
-      <div className="flex justify-between mb-4">
-        <Input placeholder="Search in list..." className="w-1/3" />
-        <div className="flex gap-2">
-          <Button variant="outline">Sort A-Z</Button>
-          <Button variant="outline">Filter</Button>
-          <Button className="bg-black text-white">+ Create Job</Button>
-        </div>
-      </div>
-      <Table>
-        <TableHeader>
-          <TableRow className="bg-gray-100">
-            <TableHead>Company</TableHead>
-            <TableHead>Job Name</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Work Type</TableHead>
-            <TableHead>Publish Date</TableHead>
-            <TableHead>Created By</TableHead>
-            <TableHead></TableHead>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Id</StyledTableCell>
+            <StyledTableCell align="left">First Name</StyledTableCell>
+            <StyledTableCell align="left">Last Name</StyledTableCell>
+            <StyledTableCell align="left">Gender</StyledTableCell>
+            <StyledTableCell align="left">Specialist</StyledTableCell>
+            <StyledTableCell align="left">Location</StyledTableCell>
           </TableRow>
-        </TableHeader>
+        </TableHead>
         <TableBody>
-          {jobs.map((job, index) => (
-            <TableRow key={index}>
-              <TableCell>{job.company}</TableCell>
-              <TableCell>{job.job}</TableCell>
-              <TableCell>
-                <span
-                  className={`px-2 py-1 text-sm rounded ${statusStyles[job.status]}`}
-                >
-                  {job.status}
-                </span>
-              </TableCell>
-              <TableCell>{job.workType}</TableCell>
-              <TableCell>{job.date}</TableCell>
-              <TableCell>{job.createdBy}</TableCell>
-              <TableCell>
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button variant="ghost" size="icon">
-                      <MoreVertical className="w-4 h-4" />
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent align="end">
-                    <DropdownMenuItem>View Details</DropdownMenuItem>
-                    <DropdownMenuItem>Edit</DropdownMenuItem>
-                    <DropdownMenuItem>Delete</DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </TableCell>
-            </TableRow>
+          {rows.map((row) => (
+            <StyledTableRow key={row.Id}>
+              <StyledTableCell component="th" scope="row">
+                {row.Id}
+              </StyledTableCell>
+              <StyledTableCell align="left">{row.First_name}</StyledTableCell>
+              <StyledTableCell align="left">{row.Last_name}</StyledTableCell>
+              <StyledTableCell align="left">{row.Gender}</StyledTableCell>
+              <StyledTableCell align="left">{row.Specialist}</StyledTableCell>
+              <StyledTableCell align="left">{row.Location}</StyledTableCell>
+            </StyledTableRow>
           ))}
         </TableBody>
       </Table>
-    </div>
+    </TableContainer>
   )
 }
-
-export default DoctorsContent
