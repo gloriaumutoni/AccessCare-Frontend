ce87ecc68debdf20fce1864a28bd9a04c1a2cb85
Gloria Umutoni <gloriaumutoni54@gmail.com>
2025-04-03 22:32:40 +0200

    Fixing file name casing

---
+++ src/components/bookings/AboutUs.tsx
@@ -0,0 +1,18 @@
+function AboutUs() {
+  return (
+    <div className="bg-[url(/nurse.png)]">
+      <div className="bg-black/30 text-white py-12 *:w-s flex flex-col space-y-8 mt-6 items-center">
+        <img src="/Vector.png" alt="" />
+        <p className="w-2xl text-center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+          placerat scelerisque tortor ornare ornare. Quisque placerat
+          scelerisque felis vitae tortor augue. Velit nascetur Consequat
+          faucibus porttitor enim et.
+        </p>
+        <hr className="w-xs text-primary-400" />
+      </div>
+    </div>
+  )
+}
+
+export default AboutUs
---
+++ src/components/bookings/Header.tsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <div className="text-center py-8">
+      <h1 className="font-bold text-4xl">
+        Search for Doctor, Make a Consultant
+      </h1>
+      <h2 className="text-center text-gray-400 text-lg pt-3">
+        Discover the best doctors, clinic, hospitals, nearest cities that suits
+        your calender
+      </h2>
+    </div>
+  )
+}
+
+export default Header
